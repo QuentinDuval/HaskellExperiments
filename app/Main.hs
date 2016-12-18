3c8e07f311212b12bd0b30f6cd2b83ec06f43f14
Duval Quentin <senmenty@gmail.com>
2016-12-18 21:18:45 +0100

    criterion

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,30 @@
 module Main where
 
 import Criterion
+import Criterion.Main(defaultMain)
 import Data.Semigroup(Min(..))
 import Lib
 import RMQ
+
+
+-- Run with:
+-- stack exec Experiments-exe
 
 main :: IO ()
 main = do
   someFunc
   testRMQ
+  defaultMain [testRMQPerf]
 
 
+testRMQPerf :: Benchmark
+testRMQPerf =
+  let rmq = fromList (map Min [1..1000000 :: Int])
+  in bgroup "RMQ" [
+      bench "Big queries" $ nf (query rmq) (Range 100 10000) -- TODO: full eval?
+    ]
+
+-- For interactive use
 testRMQ :: IO ()
 testRMQ = do
   let rmq = fromList (map Min [3, 2, 1, 3, 1, 2, 4 :: Int])
@@ -19,3 +33,5 @@ testRMQ = do
   print $ 1 == query rmq (Range 1 3)
   print $ 1 == query rmq (Range 0 100)
   print $ query (emptyRmq :: RMQ (Min Int)) (Range 2 1)
+
+  -- benchmark $ whnf (query rmq) (Range 2 5)
