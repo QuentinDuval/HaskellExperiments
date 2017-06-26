b11d51bdcec7c9bd3e39efb12a2adf88fa8b07af
Duval Quentin <senmenty@gmail.com>
2017-06-26 21:30:36 +0200

    experiments with lenses

---
+++ src/Lenses.hs
@@ -0,0 +1,96 @@
+{-# LANGUAGE RankNTypes #-}
+module Lenses where
+
+import Control.Lens
+import Data.Char (toLower)
+import qualified Data.Text as T
+import Data.Text.Lens
+
+
+test_data :: [(Int, String)]
+test_data = [(1, "STRING"), (2, "INT"), (3, "LIST")]
+
+test_data_text :: [(Int, T.Text)]
+test_data_text = test_data & traversed . _2 %~ T.pack
+
+test_data_rec :: [[(Int, String)]]
+test_data_rec = [test_data, test_data]
+
+isVowel :: Char -> Bool
+isVowel x = elem (toLower x) "aeiou"
+
+
+run_tests :: IO ()
+run_tests = do
+
+  -- Print all the elements of the list (with and without side effects)
+  print (test_data ^.. traversed)
+  test_data & traversed %%~ print
+  print =<< (test_data & traversed %%~ print)
+
+  -- Print all the second elements of the list (with and without side effects)
+  print (test_data ^.. traversed . _2)
+  test_data & traversed . _2 %%~ print
+
+  -- Edit all first elements of the list (with and without side effects)
+  print (test_data & traversed . _1 %~ (* 2))
+  print =<< (test_data & traversed . _1 %%~ (return . (* 2)))
+
+  -- Asks for the user input at every edit
+  -- print =<< (test_data & traversed . _1 %%~ (\x -> do y <- readLn; return (x * y)))
+
+  -- Change the string after the first letter toLower
+  print (test_data & traversed . _2 . dropping 1 traversed %~ toLower)
+  print (test_data_text & traversed . _2 . unpacked . dropping 1 traversed %~ toLower)
+  print (test_data_text & traversed . _2 . dropping 1 text %~ toLower)
+
+  -- Change the string after the first OVERALL letter toLower
+  print (test_data & dropping 1 (traversed . _2 . traversed) %~ toLower)
+
+  -- Reverse the strings (internally)
+  print (test_data & traversed . _2 %~ reverse)
+  print (test_data_rec & reversed . traversed . traversed . _2 %~ reverse)
+
+  -- Reverse the strings (externally)
+  -- let reversed = involuted reverse
+  print (zip (test_data ^.. traversed . _1) (reverse (test_data ^.. traversed . _2)))
+  print (zip (test_data ^.. traversed . _1) (test_data ^.. reversed . traversed . _2))
+
+  -- Reverse the strings (externally - recursive)
+  print $ test_data_rec & traversed %~ \x -> zip (x ^.. traversed . _1) (reverse (x ^.. traversed . _2))
+  -- TODO: we are in trouble if we want to reverse the whole list accross all zooms (is this what Specter allows?)
+  -- TODO: what about `backwards` in Control.Lens.Fold ??
+
+  -- Reverse the vowel letters
+  print ([1..10] & traversed . filtered even %~ (* 2))
+  print (test_data ^.. traversed . _2 . traversed . filtered isVowel)
+  -- TODO: how do you reverse the vowel internally ??? You need to be able to reconstruct the list (ISO ?)
+  -- print (test_data & traversed . _2 %~ (\s -> ))
+
+  -- Replace a sub-range with another range? It is doable, but we have to do it manually (check `srange`)
+  print ([1..10] & srange 2 5 .~ [])
+  print ([1..10] & srange 2 5 %~ reverse)
+  print ([1..10] & srange 2 5 %~ concatMap (replicate 2))
+
+  -- A list of trains with its coaches
+  let trains = [(1, "abc"), (2, "edf")]
+  print (concatMap (transposeOf _2) trains)
+  print (concat (trains & traversed %~ transposeOf _2))
+  -- print (trains ^.. transposeOf (traversed _2))
+
+  return ()
+
+srange' :: Int -> Int ->  Iso' [a] [a]
+srange' start end =
+  iso
+    (drop start . take (end - start))
+    undefined -- How to reconstruct? We do not have enough data
+
+srangeImpl :: Int -> Int ->  Iso' [a] ([a], [a], [a])
+srangeImpl start end =
+  iso
+    (\x -> (take start x, take (end - start) (drop start x) , drop end x))
+    (\(before, x, after) -> before ++ x ++ after)
+
+srange :: Int -> Int -> Lens' [a] [a]
+srange start end = srangeImpl start end . _2
