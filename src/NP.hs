488fca7501b1b917a6117fb9d1f697e82f64f7e8
Duval Quentin <senmenty@gmail.com>
2017-08-26 16:35:26 +0200

    sat

---
+++ src/NP.hs
@@ -0,0 +1,65 @@
+module NP where
+
+import Control.Monad
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+
+------------------------------------------------------------
+
+headSafe :: [a] -> Maybe a
+headSafe [] = Nothing
+headSafe xs = Just (head xs)
+
+------------------------------------------------------------
+
+type Var = String
+
+data Term
+  = Pos { getVar :: String }
+  | Neg { getVar :: String }
+
+newtype SAT = SAT { conjunctions ::  [[Term]]}
+
+collectVars :: SAT -> [Var]
+collectVars = Set.toList . Set.fromList . map getVar . concat . conjunctions
+
+evalTerm :: Map.Map Var Bool -> Term -> Bool
+evalTerm env (Pos var) = env Map.! var
+evalTerm env (Neg var) = not (env Map.! var)
+
+evalSat :: Map.Map Var Bool -> SAT -> Bool
+evalSat env = and . fmap (or . fmap (evalTerm env)) . conjunctions
+
+sat :: SAT -> Bool
+sat pb = or $ do
+  let vars = collectVars pb
+  vals <- replicate (length vars) [True, False]
+  let env = Map.fromList (zip vars vals)
+  pure $ evalSat env pb
+
+test_sat :: IO ()
+test_sat = do
+  let formula = SAT [[Pos "x1", Pos "x2", Pos "x1"]
+                    ,[Neg "x1", Neg "x2", Pos "x1"]]
+  print (sat formula)
+
+------------------------------------------------------------
+
+satExample :: Bool
+satExample =
+  or $ do
+    x1 <- [True, False]
+    x2 <- [True, False]
+    -- ...
+    xN <- [True, False]
+    pure ((x1 || x2 || xN) && (not x1 || not x2 || xN))
+
+satExample' :: Maybe [Bool]
+satExample' =
+  headSafe $ do
+    x1 <- [True, False]
+    x2 <- [True, False]
+    -- ...
+    xN <- [True, False]
+    guard ((x1 || x2 || xN) && (not x1 || not x2 || xN))
+    pure [x1, x2, xN]
