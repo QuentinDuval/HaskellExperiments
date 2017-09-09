f031124583d5a8433f94258ce7a1fe4575e845d6
Duval Quentin <senmenty@gmail.com>
2017-09-09 17:39:58 +0200

    shapes

--- src/Shape.hs
+++ src/Shape.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE TypeSynonymInstances, FlexibleInstances #-}
 module Shape where
 
+import Data.Monoid((<>))
+
 
 newtype Shape coord = Shape { isInShape :: coord -> Bool }
 
@@ -26,9 +28,49 @@ rectangle topRight@(right, top) bottomLeft@(left, bottom) =
 outside :: Shape Point2D -> Shape Point2D
 outside s = Shape (not . isInShape s)
 
-instance Monoid (Shape coord) where
-  mempty = Shape (const False)
+-- Different Monoids
+
+full :: Shape coord
+full = Shape (const True)
+
+intersect :: Shape coord -> Shape coord -> Shape coord
+intersect s1 s2 = Shape $ \coord -> all (`isInShape` coord) [s1, s2]
+
+empty :: Shape coord
+empty = Shape (const False)
+
+superpose :: Shape coord -> Shape coord -> Shape coord
+superpose s1 s2 = Shape $ \coord -> any (`isInShape` coord) [s1, s2]
+
+-- Leveraging the Monoids
+
+ring :: Point2D -> Radius -> Radius -> Shape Point2D
+ring center smallRadius bigRadius =
+  intersect
+    (circle center bigRadius)
+    (outside (circle center smallRadius))
+
+--
+
+{-
+newtype Intersection coord = Intersection { unIntersect :: Shape coord }
+newtype Superposition coord = Superposition { unSuperpose :: Shape coord }
+
+intersection :: (coord -> Bool) -> Intersection coord
+intersection = Intersection . Shape
+
+superposition :: (coord -> Bool) -> Superposition coord
+superposition = Superposition . Shape
+
+instance Monoid (Intersection coord) where
+  mempty = intersection (const True)
   mappend s1 s2 = mconcat [s1, s2]
-  mconcat shapes = Shape $ \coord -> any (`isInShape` coord) shapes
+  mconcat shapes = intersection $ \coord -> all (\(Intersection s) -> isInShape s coord) shapes
+
+instance Monoid (Superposition coord) where
+  mempty = superposition (const False)
+  mappend s1 s2 = mconcat [s1, s2]
+  mconcat shapes = superposition $ \coord -> any (\(Superposition s) -> isInShape s coord) shapes
+-}
 
 --
