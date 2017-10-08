2e1a3b0017ac1eb3836310f79c69d003f2cbdac7
Duval Quentin <senmenty@gmail.com>
2017-10-08 19:20:12 +0200

    with proxy type

--- src/DepTypes.hs
+++ src/DepTypes.hs
@@ -2,6 +2,8 @@
 module DepTypes () where
 
 
+-- Non injective type function
+
 class Func a where
   type Res a :: *
 
@@ -12,8 +14,18 @@ instance Func String where
   type Res String = String
 
 -- Does not compile ! Cannot deduce a
--- f :: Res a -> ()
--- f _ = ()
+-- f :: Res a -> IO ()
+-- f _ = print "Does not compile (type `a` is ambiguous)"
+
+-- Providing hints to the compiler
+
+data Proxy a = Proxy
+
+f :: Proxy a -> Res a -> IO ()
+f _ _ = print "Useless function, but compiles fine"
+
+
+-- Injective type function
 
 class FuncInj a where
   data ResInj a :: *
