96b44d57ed5b54b9a9aecfc3944793eb2ea41d23
Duval Quentin <senmenty@gmail.com>
2017-10-08 14:58:46 +0200

    a snail story

---
+++ src/Writer.hs
@@ -0,0 +1,76 @@
+{-# LANGUAGE FlexibleInstances #-}
+module Writer where
+
+
+-- Writer Monad (no side effect, just accumulate)
+
+data Writer m a
+  = Writer { writerLog :: m, writerOut :: a }
+  deriving (Show)
+
+instance Functor (Writer m) where
+  fmap f (Writer m a) = Writer m (f a)
+
+instance (Monoid m) => Applicative (Writer m) where
+  pure = Writer mempty
+  Writer m1 f <*> Writer m2 a = Writer (mappend m1 m2) (f a)
+
+instance (Monoid m) => Monad (Writer m) where
+  Writer m1 a >>= f = let Writer m2 b = f a in Writer (mappend m1 m2) b
+
+--
+
+class Monad m => MonadLog m where
+  logInfo :: String -> m ()
+
+instance MonadLog (Writer String) where
+  logInfo s = Writer (s ++ "\n") ()
+
+--
+
+type Snail = String
+
+popSnail :: (MonadLog m) => String -> m Snail
+popSnail name = do
+  logInfo $ "New snail created: " ++ name
+  pure name
+
+sendDart :: (MonadLog m) => Snail -> Snail -> m Bool
+sendDart s1 s2 = do
+  logInfo $ "Snail " ++ s1 ++ " sends love dart to " ++ s2
+  pure True
+
+parentalAdvisorySnailStuff :: (MonadLog m) => Snail -> Snail -> m Bool
+parentalAdvisorySnailStuff s1 s2 = do
+  logInfo $ "Snails " ++ s1 ++ " and " ++ s2 ++ " just did it."
+  pure True
+
+andM :: (Monad m) => [m Bool] -> m Bool
+andM [] = pure True
+andM (mb:mbs) = do
+  b <- mb
+  if b then andM mbs else pure b
+
+mateSnails :: (MonadLog m) => m Bool
+mateSnails = do
+  slug <- popSnail "Slug"
+  slim <- popSnail "Slim"
+  andM [
+    sendDart slug slim ,
+    sendDart slim slug ,
+    parentalAdvisorySnailStuff slug slim ]
+
+{-
+  ok1 <-
+  ok2 <- sendDart slim slug
+  ok3 <- parentalAdvisorySnailStuff slug slim
+  pure (ok1 && ok2 && ok3)
+-}
+
+--
+
+run_test :: IO ()
+run_test = do
+  putStrLn $ writerLog mateSnails
+
+--
