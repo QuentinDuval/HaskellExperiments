142f99b0543cb2db15177f57b63a4f23f25c7023
Quentin Duval <QuentinDuval@users.noreply.github.com>
2018-03-07 18:17:53 +0100

    Update SocialMedia.hs

--- src/SocialMedia.hs
+++ src/SocialMedia.hs
@@ -65,20 +65,20 @@ data BlogPost = BlogPost -- Would add a postId, but useless here
   , postLikes :: Int
   } deriving (Show, Eq, Ord)
 
-class Monad m => IRepository m where
+class Monad m => WithProfileInfo m where
   friendsOf :: ProfileId -> m [ProfileId]
   subjectsOf :: ProfileId -> m (Set Subject)
   lastPostsOf :: ProfileId -> m [BlogPost]
 
 -- This code is embarded in a "onConnection" use case... you cannot just separate effects
-getSuggestedPosts :: (IRepository m) => ProfileId -> m [BlogPost]
+getSuggestedPosts :: (WithProfileInfo m) => ProfileId -> m [BlogPost]
 getSuggestedPosts userId = do
     friendIds <- friendsOf userId
     subjects <- subjectsOf userId
     friendsPosts <- forM friendIds $ \friendId -> do
         posts <- lastPostsOf friendId
         return (filter (isAbout subjects) posts)
-    return $ mostLiked 3 (concat friendsPosts)
+    return (mostLiked 3 (concat friendsPosts))
 
 isAbout :: (Set Subject) -> BlogPost -> Bool
 isAbout subjects subject = Set.member (postSubject subject) subjects
@@ -91,7 +91,7 @@ mostLiked n = take n . reverse . sortOn postLikes
 -- Infrastructure code (in memory)
 --------------------------------------------------------------------------------
 
-data InMemoryDataSource = InMemoryDataSource
+data LocalProfileInfo = LocalProfileInfo
     { friendIds_ :: Map ProfileId [ProfileId]
     , subjects_ :: Map ProfileId (Set Subject)
     , lastPosts_ :: Map ProfileId [BlogPost]
@@ -99,9 +99,9 @@ data InMemoryDataSource = InMemoryDataSource
 
 -- With such a threading of the state, no parallelization is possible
 data InMemoryRepository a = InMemoryRepository
-    { runInMemory :: InMemoryDataSource -> (a, InMemoryDataSource) }
+    { runInMemory :: LocalProfileInfo -> (a, LocalProfileInfo) }
 
-withInMemoryDb :: InMemoryDataSource -> InMemoryRepository a -> a
+withInMemoryDb :: LocalProfileInfo -> InMemoryRepository a -> a
 withInMemoryDb db f = fst $ runInMemory f db
 
 instance Monad InMemoryRepository where
@@ -117,7 +117,7 @@ instance Applicative InMemoryRepository where
     pure = return
     (<*>) = ap
 
-instance IRepository InMemoryRepository where
+instance WithProfileInfo InMemoryRepository where
     friendsOf profileId = InMemoryRepository $ \db ->
         (Map.findWithDefault [] profileId (friendIds_ db), db)
     subjectsOf profileId = InMemoryRepository $ \db ->
@@ -137,48 +137,42 @@ data Cache = Cache
     }
 
 -- The MVar is needed VS state monad to avoid sequential applicatives
-data ProductionRepository a = ProductionRepository
-    { runProduction :: MVar Cache -> IO (MVar a) }
+data RemoteProfileInfo a = RemoteProfileInfo
+    { runRemoteProfileInfo :: MVar Cache -> IO (MVar a) }
 
-withProduction :: Cache -> ProductionRepository a -> IO a
-withProduction db f = do
+withRemoteProfileInfo :: Cache -> RemoteProfileInfo a -> IO a
+withRemoteProfileInfo db f = do
     dbVar <- newMVar db
-    var <- runProduction f dbVar
-    await var
+    await =<< runRemoteProfileInfo f dbVar
 
-instance Functor ProductionRepository where
-    -- Chaining futures
-    fmap f m = ProductionRepository $ \db -> do
-        var <- runProduction m db
+instance Functor RemoteProfileInfo where
+    fmap f m = RemoteProfileInfo $ \db -> do -- Chaining futures
+        var <- runRemoteProfileInfo m db
         chain var f
 
-instance Applicative ProductionRepository where
-    pure a = ProductionRepository $ \db -> sync (pure a)
-    -- Without parallel procesing
-    -- (<*>) = ap
-    -- With parallel processing
-    mf <*> ma = ProductionRepository $ \db -> do
-        fVar <- async $ runProduction mf db -- TODO: mf may block... find another way?
-        aVar <- async $ runProduction ma db
+instance Applicative RemoteProfileInfo where
+    pure a = RemoteProfileInfo $ \db -> sync (pure a)
+    -- (<*>) = ap -- Without parallel procesing
+    mf <*> ma = RemoteProfileInfo $ \db -> do       -- With parallel processing
+        fVar <- async $ runRemoteProfileInfo mf db  -- TODO: mf may block! find another way?
+        aVar <- async $ runRemoteProfileInfo ma db
         async $ do
             f <- await =<< await fVar
             a <- await =<< await aVar
             pure (f a)
 
-instance Monad ProductionRepository where
-    -- Sequential processing needed
-    ra >>= f = ProductionRepository $ \db -> do
-        var <- runProduction ra db
-        a <- await var
-        runProduction (f a) db
+instance Monad RemoteProfileInfo where
+    ra >>= f = RemoteProfileInfo $ \db -> do        -- Sequential processing needed
+        a <- await =<< runRemoteProfileInfo ra db
+        runRemoteProfileInfo (f a) db
 
-instance MonadIO ProductionRepository where
-    liftIO io = ProductionRepository $ \db -> sync io
+instance MonadIO RemoteProfileInfo where
+    liftIO io = RemoteProfileInfo $ \db -> sync io
 
-instance IRepository ProductionRepository where
-    friendsOf profileId = ProductionRepository $ \dbVar -> fetchFriends dbVar profileId
-    subjectsOf profileId = ProductionRepository $ \dbVar -> fetchSubjects dbVar profileId
-    lastPostsOf profileId = ProductionRepository $ \dbVar -> fetchLastPosts dbVar profileId
+instance WithProfileInfo RemoteProfileInfo where
+    friendsOf profileId = RemoteProfileInfo $ \dbVar -> fetchFriends dbVar profileId
+    subjectsOf profileId = RemoteProfileInfo $ \dbVar -> fetchSubjects dbVar profileId
+    lastPostsOf profileId = RemoteProfileInfo $ \dbVar -> fetchLastPosts dbVar profileId
 
 fetchAndCache :: (Ord k) => Map k (MVar v) -> k -> (k -> IO (MVar v)) -> IO (MVar v, Map k (MVar v))
 fetchAndCache cache key f =
@@ -234,15 +228,17 @@ httpGetLastPosts profileId = async $ do
 -- Test
 --------------------------------------------------------------------------------
 
-inMemoryDb :: InMemoryDataSource
-inMemoryDb = InMemoryDataSource
-              { friendIds_ = Map.fromList [(1, [1, 2, 3]), (2, [1, 2, 3]), (3, [1, 2, 3])]
+inMemoryDb :: LocalProfileInfo
+inMemoryDb = LocalProfileInfo
+              { friendIds_ = Map.fromList [ (1, [1, 2, 3])
+                                          , (2, [1, 2, 3])
+                                          , (3, [1, 2, 3]) ]
               , subjects_ = Map.fromList [ (1, Set.fromList["C++", "Java"])
                                          , (2, Set.fromList["C++", "Haskell"])
                                          , (3, Set.fromList["Clojure", "Haskell"])]
               , lastPosts_ = Map.fromList [ (1, [BlogPost "C++" 15, BlogPost "Java" 10] )
                                           , (2, [BlogPost "Haskell" 15, BlogPost "C++" 5] )
-                                          , (3, [BlogPost "Java" 20])
+                                          , (3, [BlogPost "Java" 20, BlogPost "Haskell" 5])
                                           ]}
 
 socialMediaTest :: IO ()
@@ -253,11 +249,11 @@ socialMediaTest = do
         pure (t1, t2)
 
     let cache = Cache { cachedFriendIds_ = Map.empty, cachedSubjects_ = Map.empty, cachedLastPosts_ = Map.empty }
-    withProduction cache $ do
+    withRemoteProfileInfo cache $ do
         t1 <- getSuggestedPosts 1
         t2 <- getSuggestedPosts 2
         liftIO (print (t1, t2))
-    withProduction cache $ do -- with a new cache
+    withRemoteProfileInfo cache $ do -- with a new cache
         t1 <- getSuggestedPosts 2
         t2 <- getSuggestedPosts 3
         liftIO (print (t1, t2))
